b244bfb0eeae268a38ca91922eb4f6648021b18b
javascriptninja815 <xandokan815@protonmail.com>
2018-11-29 22:22:26 -0800

    integrated d3 v5+ vue-cli

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <tree/>
+  </div>
+</template>
+
+<script>
+import tree from './components/tree.vue'
+
+export default {
+  name: 'app',
+  components: {
+    tree
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
